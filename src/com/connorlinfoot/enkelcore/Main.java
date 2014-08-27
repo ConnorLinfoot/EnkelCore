9cea3424afd881f8d2dc9e61329dcd005c651972
Connor Linfoot <connorlinfoot@me.com>
2014-08-27 14:52:29 +0100

    Initial commit

---
+++ src/com/connorlinfoot/enkelcore/Main.java
@@ -0,0 +1,33 @@
+package com.connorlinfoot.enkelcore;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin implements Listener {
+    private static Plugin instance;
+
+    public void onEnable() {
+        instance = this;
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        Server server = getServer();
+        ConsoleCommandSender console = server.getConsoleSender();
+
+        console.sendMessage(ChatColor.GREEN + "============ EnkelCore! ============");
+        console.sendMessage(ChatColor.GREEN + "=========== VERSION: 1.0 ===========");
+        console.sendMessage(ChatColor.GREEN + "======== BY CONNOR LINFOOT! ========");
+    }
+
+    public void onDisable() {
+        getLogger().info(getDescription().getName() + " has been disabled!");
+    }
+
+    public static Plugin getInstance() {
+        return instance;
+    }
+}
